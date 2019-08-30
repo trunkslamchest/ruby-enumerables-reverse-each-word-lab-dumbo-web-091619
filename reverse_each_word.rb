6b383f3752d74c603f840cbdb60e0a494eba04f1
Austin Smith <trunk9slamchest@gmail.com>
2019-08-30 06:29:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,4 @@
-# sentence1 = "Hello there, and how are you?"
-# sentence2 = "Hi again, just making sure it's reversed!"
-
 def reverse_each_word(sentence)
   split = sentence.split(" ").collect { |e| e.reverse }
   return split.join(" ")
 end
-
-# reverse_each_word(sentence1)
-# reverse_each_word(sentence2)
